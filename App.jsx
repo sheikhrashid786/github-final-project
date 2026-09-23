2162d81599df725680b18cf130e5b27337acdafc
Sheikh  Rashid <58813506+sheikhrashid786@users.noreply.github.com>
2026-09-23 13:04:27 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
-    <div className="landing-page">
-      <h1>Paradise Nursery</h1>
-      <p>Welcome to Paradise Nursery - your destination for beautiful plants.</p>
-      <button>Get Started</button>
+    <div>
+      {!showProducts ? (
+        <div className="landing-page">
+          <h1>Paradise Nursery</h1>
+          <p>
+            Welcome to Paradise Nursery - your destination for beautiful plants.
+          </p>
+
+          <button onClick={() => setShowProducts(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
