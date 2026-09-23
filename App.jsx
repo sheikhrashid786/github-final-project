818f288b5669faf314f546e6297c635fe0270ed2
Sheikh  Rashid <58813506+sheikhrashid786@users.noreply.github.com>
2026-09-23 12:46:00 +0500

    Create initial App component for landing page

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery - your destination for beautiful plants.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
